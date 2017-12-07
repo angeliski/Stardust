00000fbad147cc5262329d5f0bc67b78fe9ca5ba
Rogerio Angeliski <angeliski@hotmail.com>
2017-12-07 20:09:53 -0200

    Init project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to stardust
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/stardust/core_test.clj
@@ -0,0 +1,7 @@
+(ns stardust.core-test
+  (:require [clojure.test :refer :all]
+            [stardust.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
